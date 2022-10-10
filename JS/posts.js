c06806e1f7afae09b397b9f272a692008070d573
johansen98 <61602808+johansen98@users.noreply.github.com>
2022-10-10 20:04:01 +0200

    made post funktonality and fixed style

--- JS/posts.js
+++ JS/posts.js
@@ -1,5 +1,13 @@
+let page = 0;
+getPosts(page);
 
-function getPosts(){
+window.onscroll = function(e) {
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      getPosts(page++)
+  }
+};
+
+function getPosts(page){
     const headers = new Headers();
     const token = localStorage.getItem("token");
     headers.append("Content-Type", "application/json");
@@ -10,7 +18,7 @@ function getPosts(){
       headers: headers
     };
 
-    fetch('https://nf-api.onrender.com/api/v1/social/posts', request)
+    fetch('https://nf-api.onrender.com/api/v1/social/posts?_author=true&limit=20&offset=' + page, request)
     .then((response) => response.json())
     .then((data) => onResponse(data))
 }
@@ -18,9 +26,8 @@ function getPosts(){
 function onResponse(data){
 
     const posts = data;
+    const postConatiner = document.getElementById('postContainer');
     posts.forEach(post => {
-      
-          const postConatiner = document.getElementById('postContainer');
       
           const postCardContainer = document.createElement('div');
           const postCard = document.createElement('div');
@@ -31,12 +38,52 @@ function onResponse(data){
           const postFooter = document.createElement('div');
           const authorImg = document.createElement('img');
           const authorName = document.createElement('small')
-          const authorDOB = document.createElement('small')
+          const postCreatedDate = document.createElement('small')
 
-          postImg.src = post.media[0]
+          postCardContainer.classList.add('col');
+          postCardContainer.classList.add('mb-2');
+          postCard.classList.add('h-100')
+          postCard.classList.add('card')
+          
+          postImg.src = post.media
+          postImg.classList.add('card-img-top')
+          postImg.classList.add('w-50')
 
-          console.log(postImg)
+          postBody.classList.add('card-body')
 
+          postTittle.innerHTML = post.tittle 
+          postTittle.classList.add('card-title')
+          postTittle.classList.add('text-center')
+
+          postContent.innerHTML = post.body
+          postContent.classList.add('card-text')
+
+          postFooter.classList.add('card-footer')
+
+          authorImg.src = post.author.avatar
+          authorImg.classList.add('footer-image')
+
+          authorName.innerHTML = post.author.name
+          authorName.classList.add('text-muted')
+
+          postCreatedDate.innerHTML = post.created
+          postCreatedDate.classList.add('text-muted')
+
+          postCardContainer.appendChild(postCard)
+          postCard.appendChild(postImg)
+          postCard.appendChild(postBody)
+          postCard.appendChild(postTittle)
+          postCard.appendChild(postContent)
+          postCard.appendChild(postFooter)
+          postFooter.appendChild(authorImg)
+          postFooter.appendChild(authorName)
+          postFooter.appendChild(postCreatedDate)
+          
+          postConatiner.appendChild(postCardContainer)
+
+
+
+          
       
     });
 
